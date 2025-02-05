57dcea3cf329ffa3eaf23d12afa94609e3651e11
Mart305 <122917215+Mart305@users.noreply.github.com>
2025-02-05 12:49:54 -0600

    Q3 Done

---
+++ VG1_RiveraMartin/Assets/Code/Platformer/Projectile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+namespace Platformer
+{
+    public class Projectile : MonoBehaviour
+    {
+        Rigidbody2D _rigidbody2D;
+        void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _rigidbody2D.velocity = transform.right * 10f;
+        }
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
